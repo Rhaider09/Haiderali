8dd456e90262598dc634cb713edd65b1a02dfde0
haiderali <haiderali226433@gmil.com>
2025-06-09 15:35:53 +0500

    update

--- resources/views/layouts/inc/app.blade.php
+++ resources/views/layouts/inc/app.blade.php
@@ -25,6 +25,7 @@
     </div>
     @include('layouts.inc.scripts')
     @include('layouts.inc.admin.flashmessagefrontend')
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 </body>
 
 </html>
